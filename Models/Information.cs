b5242c52bfabf1ca1f98d580fdd11a1fd320cedd
Nicolas Tran <57213812+n-tran117@users.noreply.github.com>
2023-05-30 14:13:14 +0200

    Initial commit

---
+++ Models/Information.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class Information
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Forename { get; set; }
+        [Required(ErrorMessage = "Select a gender")]
+        public string Gender { get; set; }
+        [MinLength(5)]
+        public string Address { get; set; }
+        public string Town { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [RegularExpression("\\d{5}")]
+        public int ZipCode { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public string Training { get; set; }
+        public string? CobolRev { get; set; }
+        public string? TrainRev { get; set; }
+    }
+}
---
+++ Models/PersonnalInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TPLOCAL1.Models
+{
+    public class PersonnalInfo
+    {
+
+
+    }
+}
